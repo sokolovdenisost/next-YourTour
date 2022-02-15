021aa68067aedb78cd0732d73db80ad0939a529c
6y6exa <sokolovdenisost@gmail.com>
2022-02-15 16:30:26 +0300

    change headline

--- components/common/Headlines/Headlines.jsx
+++ components/common/Headlines/Headlines.jsx
@@ -3,29 +3,8 @@ import PropTypes from "prop-types";
 import classNames from "classnames";
 import s from "./Headlines.module.scss";
 
-export const Headline = ({ children, className, size }) => {
-  switch (size) {
-    case "h1":
-      return <h1 className={classNames(s.h1, className)}>{children}</h1>;
-
-    case "h2":
-      return <h2 className={classNames(s.h2, className)}>{children}</h2>;
-
-    case "h3":
-      return <h3 className={classNames(s.h3, className)}>{children}</h3>;
-
-    case "h4":
-      return <h4 className={classNames(s.h4, className)}>{children}</h4>;
-
-    case "h5":
-      return <h5 className={classNames(s.h5, className)}>{children}</h5>;
-
-    case "h6":
-      return <h6 className={classNames(s.h6, className)}>{children}</h6>;
-
-    default:
-      return;
-  }
+export const Headline = ({ children, className, size: Tag }) => {
+  return <Tag className={classNames(s[Tag], className)}>{children}</Tag>;
 };
 
 Headline.propTypes = {
